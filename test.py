cfa77d1f4d34214056890a59834fb22cd6c4d2d0
buakaew <65310167@go.buu.ac.th>
2024-10-02 13:56:04 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+print("welcome to Python By Buakaew")
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end=' ')
+    for j in range(i,n):
+        print('*',end=' ')
+    print()
